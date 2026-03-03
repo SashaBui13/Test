61d7e0bd2282b0751fd81346bdb2fb8e74287989
SaBcdng <sasha.bui.02.13@gmail.com>
2026-03-03 08:56:38 +0200

    #TES-15
    create connection to database

---
+++ TESTPROJECT/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using TESTPROJECT.Models;
+
+namespace TESTPROJECT.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+   
+    }
+}
